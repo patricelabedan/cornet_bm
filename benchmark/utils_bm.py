2791e584c23c0f7a5693874430d6fcfd20517897
Patrice LABEDAN <patrice.labedan@isae.fr>
2025-11-27 19:32:11 +0100

    Moving functions save_distance_matrix_to_csv() and sort_csv_by_distance() in accadil_bm

--- benchmark/utils_bm.py
+++ benchmark/utils_bm.py
@@ -5,29 +5,6 @@ import csv
 import shutil
 import os
 
-def save_distance_matrix_to_csv(distance_matrix_path, ref_pic_list_path, output_csv_path):
-    distances = np.load(distance_matrix_path)
-    with open(ref_pic_list_path, mode='r', encoding='utf-8') as f:
-        ref_pic_list = [line.strip() for line in f if line.strip()]
-    with open(output_csv_path, mode='w', newline='', encoding='utf-8') as csvfile:
-        writer = csv.writer(csvfile, delimiter=';')
-        writer.writerow(['name1', 'name2', 'Distance'])
-        n = len(ref_pic_list)
-        for i in range(n):
-            for j in range(i+1, n):
-                writer.writerow([ref_pic_list[i], ref_pic_list[j], distances[i, j]])
-
-
-def sort_csv_by_distance(input_csv_path, output_csv_path):
-    with open(input_csv_path, mode='r', encoding='utf-8') as infile:
-        reader = csv.DictReader(infile, delimiter=';')
-        rows = list(reader)
-        rows.sort(key=lambda x: float(x['Distance']))
-
-    with open(output_csv_path, mode='w', newline='', encoding='utf-8') as outfile:
-        writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames, delimiter=';')
-        writer.writeheader()
-        writer.writerows(rows)
 
 def get_images_in_treasure_dataset(treasure, dataset):
 
